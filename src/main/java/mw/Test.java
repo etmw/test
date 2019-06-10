37eab8d7f490496c84da693a26a1065d6cfc09a0
etmw <selfwalk@163.com>
2019-06-10 17:01:31 +0800

    222

--- src/main/java/mw/Test.java
+++ src/main/java/mw/Test.java
@@ -1,5 +1,24 @@
 package mw;
 
-public class Test {
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
+public class Test {
+	public static void main(String[] args) {
+		ExecutorService ex=Executors.newSingleThreadExecutor();
+		
+		for(int i=0;i<5;i++) {
+			ex.submit(new Runnable() {
+				
+				@Override
+				public void run() {
+					for(int j=0;j<10;j++) {
+						System.out.println(Thread.currentThread().getName()+"*"+j);
+					}
+					
+				}
+			});
+		}
+		ex.shutdown();
+	}	
 }
